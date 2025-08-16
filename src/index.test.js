3b7c8626a4b668ef264fd91108e7df08e557519d
Denis Bendrikov <Denis.Bendrikov@gmail.com>
2025-08-17 00:46:44 +0300

    refactor: use named exports in tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,11 @@
 import { describe, it } from 'node:test';
 import { expect } from 'chai';
-import * as dota2Heroes from 'dota2-heroes';
+import { all, random } from 'dota2-heroes';
 
 describe('dota2-heroes', () => {
     describe('all', () => {
         it('should be an array of strings', () => {
-            expect(dota2Heroes.all).to.satisfy(isArrayOfStrings);
+            expect(all).to.satisfy(isArrayOfStrings);
 
             function isArrayOfStrings(array) {
                 return array.every(item => typeof item === 'string');
@@ -13,72 +13,72 @@ describe('dota2-heroes', () => {
         });
 
         it('should contain `Underlord`', () => {
-            expect(dota2Heroes.all).to.include('Underlord');
+            expect(all).to.include('Underlord');
         });
 
         it('should contain `Dark Willow`', () => {
-            expect(dota2Heroes.all).to.include('Dark Willow');
+            expect(all).to.include('Dark Willow');
         });
 
         it('should contain `Pangolier`', () => {
-            expect(dota2Heroes.all).to.include('Pangolier');
+            expect(all).to.include('Pangolier');
         });
 
         it('should contain `Grimstroke`', () => {
-            expect(dota2Heroes.all).to.include('Grimstroke');
+            expect(all).to.include('Grimstroke');
         });
 
         it('should contain `Mars`', () => {
-            expect(dota2Heroes.all).to.include('Mars');
+            expect(all).to.include('Mars');
         });
 
         it('should contain `Snapfire`', () => {
-            expect(dota2Heroes.all).to.include('Snapfire');
+            expect(all).to.include('Snapfire');
         });
 
         it('should contain `Void Spirit`', () => {
-            expect(dota2Heroes.all).to.include('Void Spirit');
+            expect(all).to.include('Void Spirit');
         });
 
         it('should contain `Hoodwink`', () => {
-            expect(dota2Heroes.all).to.include('Hoodwink');
+            expect(all).to.include('Hoodwink');
         });
 
         it('should contain `Dawnbreaker`', () => {
-            expect(dota2Heroes.all).to.include('Dawnbreaker');
+            expect(all).to.include('Dawnbreaker');
         });
 
         it('should contain `Marci`', () => {
-            expect(dota2Heroes.all).to.include('Marci');
+            expect(all).to.include('Marci');
         });
 
         it('should contain `Primal Beast`', () => {
-            expect(dota2Heroes.all).to.include('Primal Beast');
+            expect(all).to.include('Primal Beast');
         });
 
         it('should contain `Muerta`', () => {
-            expect(dota2Heroes.all).to.include('Muerta');
+            expect(all).to.include('Muerta');
         });
 
         it('should contain `Ringmaster`', () => {
-            expect(dota2Heroes.all).to.include('Ringmaster');
+            expect(all).to.include('Ringmaster');
         });
 
         it('should contain 125 heroes', () => {
-            expect(dota2Heroes.all).to.have.length(125);
+            expect(all).to.have.length(125);
         });
     });
 
     describe('random', () => {
-        it('should return a random item from the dota2Heroes.all', () => {
-            const randomItem = dota2Heroes.random();
-            expect(dota2Heroes.all).to.include(randomItem);
+        it('should return a random hero', () => {
+            const randomItem = random();
+            expect(all).to.include(randomItem);
         });
 
-        it('should return an array of random items if passed a number', () => {
-            const randomItems = dota2Heroes.random(3);
+        it('should return an array of random heroes if passed a number', () => {
+            const randomItems = random(3);
             expect(randomItems).to.have.length(3);
-            randomItems.forEach(item => expect(dota2Heroes.all).to.include(item));
+            randomItems.forEach(item => expect(all).to.include(item));
         });
     });
 });
